1c0a2806ec6fa4334d32d44ede66edd7f09cd121
ViktorKondrashin <visktor.srgeev@mail.ru>
2025-05-13 09:57:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,94 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int binarySearch(int len, int member_couple, int* arr, int low) {
+  int high = len - 1;
+  int middle;
+  while (low <= high) {
+    middle = (low + high) / 2;
+    if (member_couple == arr[middle]) {
+      return middle;
+    } else if (arr[middle] > member_couple) {
+      high = middle - 1;
+    } else {
+      low = middle + 1;
+    }
+  }
+  return -1;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+
+int countPairs1(int* arr, int len, int value) {
+  int kol = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        ++kol;
+      }
+    }
+  }
+  return kol;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+
+int countPairs2(int* arr, int len, int value) {
+  size_t left = 0;
+  size_t right = len - 1;
+  int pairs = 0;
+
+  while (left < right) {
+    int s = arr[left] + arr[right];
+    if (s < value) {
+      ++left;
+    } else if (s > value) {
+      --right;
+    } else {
+      if (arr[left] != arr[right]) {
+        size_t lcnt = 1;
+        size_t rcnt = 1;
+        while (left + lcnt < right && arr[left + lcnt] == arr[left]) {
+          ++lcnt;
+        }
+        while (left + lcnt - 1 < right - rcnt &&
+               arr[right - rcnt] == arr[right]) {
+          ++rcnt;
+        }
+        pairs += lcnt * rcnt;
+        left += lcnt;
+        right -= rcnt;
+      } else {
+        int k = right - left + 1;
+        pairs += k * (k - 1) / 2;
+        break;
+      }
+    }
+  }
+  return pairs;
+}
+
+int countPairs3(int* arr, int len, int value) {
+  int count_couple = 0;
+  int member_couple;
+  int middle;
+  for (int i = 0; i < len; ++i) {
+    member_couple = value - arr[i];
+    if (member_couple < arr[i]) {
+      break;
+    }
+    middle = binarySearch(len, member_couple, arr, i + 1);
+    if (middle == -1) {
+      continue;
+    } else {
+      ++count_couple;
+      int temp_middle_increase = middle;
+      int temp_middle_decrease = middle;
+      while (temp_middle_increase + 1 < len &&
+             arr[temp_middle_increase + 1] == member_couple) {
+        ++temp_middle_increase;
+        ++count_couple;
+      }
+      while (temp_middle_decrease - 1 > i &&
+             arr[temp_middle_decrease - 1] == member_couple) {
+        --temp_middle_decrease;
+        ++count_couple;
+      }
+    }
+  }
+  return count_couple;
 }
